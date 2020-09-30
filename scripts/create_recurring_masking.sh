c50a463181556b641e43c59be3170a765848d965
Ori Ram <ramori9@gmail.com>
2020-09-30 14:55:48 +0300

    Script for creating recurring span masking data

---
+++ scripts/create_recurring_masking.sh
@@ -0,0 +1,15 @@
+python create_pretraining_data.py \
+--input_file=../data/wiki/text/*/*wiki* \
+--output_dir=gs://span-pretraining/data/recurring_tfrecords \
+--vocab_file=bert-cased-vocab.txt \
+--do_lower_case=False \
+--do_whole_word_mask=False \
+--max_seq_length=512 \
+--max_predictions_per_seq=80 \
+--num_processes=63 \
+--masked_lm_prob=0.15 \
+--dupe_factor=5 \
+--geometric_masking_p=0.2 \
+--max_span_length=10 \
+--recurring_span_masking=True \
+--max_recurring_predictions_per_seq=40
