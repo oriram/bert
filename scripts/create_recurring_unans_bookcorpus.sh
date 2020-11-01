274ccce9b3e7fa94d15ca33075deb524237dfc64
Ori Ram <ramori9@gmail.com>
2020-11-01 23:45:45 +0200

    BookCorpus with unanswerable script

---
+++ scripts/create_recurring_unans_bookcorpus.sh
@@ -0,0 +1,16 @@
+python create_pretraining_data.py \
+--input_file=../bookcorpus/split_books_*.train \
+--output_dir=gs://ori-span-pretraining/data/recurring_unans_bookcorpus_tfrecords \
+--vocab_file=vocabs/bert-cased-vocab.txt \
+--do_lower_case=False \
+--do_whole_word_mask=False \
+--max_seq_length=512 \
+--max_predictions_per_seq=60 \
+--num_processes=63 \
+--masked_lm_prob=0.15 \
+--dupe_factor=5 \
+--geometric_masking_p=0.2 \
+--max_span_length=10 \
+--recurring_span_masking=True \
+--max_recurring_predictions_per_seq=30 \
+--unanswerable_prob=0.2
