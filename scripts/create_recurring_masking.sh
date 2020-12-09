d7550c998785ded9e70005763735f44c927fb0d0
Ori Ram <ramori9@gmail.com>
2020-12-09 16:18:22 +0200

    Updated data creation script for recurring

--- scripts/create_recurring_masking.sh
+++ scripts/create_recurring_masking.sh
@@ -1,12 +1,12 @@
 python create_pretraining_data.py \
---input_file=../data/wiki/text/*/*wiki* \
---output_dir=gs://span-pretraining/data/recurring_tfrecords \
+--input_file=../wiki/enwiki/*/*wiki*,../bookcorpus/split_books_*.train \
+--output_dir=gs://ori-span-pretraining/data/recurring_tfrecords_new \
 --vocab_file=vocabs/bert-cased-vocab.txt \
 --do_lower_case=False \
 --do_whole_word_mask=False \
 --max_seq_length=512 \
 --max_predictions_per_seq=60 \
---num_processes=63 \
+--num_processes=48 \
 --masked_lm_prob=0.15 \
 --dupe_factor=5 \
 --geometric_masking_p=0.2 \
